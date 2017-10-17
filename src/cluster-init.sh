bce28dd838df3efc3918324a43ed0a18b5b5b6c0
rickoliveira1 <rc.ricardosantos@outlook.com>
2017-10-17 16:15:54 -0200

    Update cluster-init.sh

--- src/cluster-init.sh
+++ src/cluster-init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-NODE1_IP=$(/usr/bin/getent hosts redis-cluster-node01 | awk {' print $1 '})
-NODE2_IP=$(/usr/bin/getent hosts redis-cluster-node02 | awk {' print $1 '})
-NODE3_IP=$(/usr/bin/getent hosts redis-cluster-node03 | awk {' print $1 '})
-NODE1=$NODE1_IP":6379"
-NODE2=$NODE2_IP":6379"
-NODE3=$NODE3_IP":6379"
+#NODE1_IP=$(/usr/bin/getent hosts redis-cluster-node01 | awk {' print $1 '})
+#NODE2_IP=$(/usr/bin/getent hosts redis-cluster-node02 | awk {' print $1 '})
+#NODE3_IP=$(/usr/bin/getent hosts redis-cluster-node03 | awk {' print $1 '})
+NODE1="redis-cluster-node01:6379"
+NODE2="redis-cluster-node02:6379"
+NODE3="redis-cluster-node03:6379"
 
 sleep 90
 
