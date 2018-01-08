7bb4960c996d601bebbc991943b2b7b28bbed71a
HieuVQ <33409484+HieuVQ@users.noreply.github.com>
2018-01-08 13:45:56 +0700

    Update cluster-init.sh

--- src/cluster-init.sh
+++ src/cluster-init.sh
@@ -6,9 +6,12 @@
 NODE1="redis-cluster-node01:6379"
 NODE2="redis-cluster-node02:6379"
 NODE3="redis-cluster-node03:6379"
+NODE4="redis-cluster-node04:6379"
+NODE5="redis-cluster-node05:6379"
+NODE6="redis-cluster-node06:6379"
 
 sleep 90
 
-/usr/local/bin/redis-trib.rb create $NODE1 $NODE2 $NODE3
+/usr/local/bin/redis-trib.rb create --replicas 1 $NODE1 $NODE2 $NODE3 $NODE4 $NODE5 $NODE6
 echo RICARDO
 exit 0
